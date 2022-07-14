16fa54d265c325ad5c2ed5b19a19b611e5a8371e
Vishal Ribdiya <vishal.ribdiya@infyom.com>
2022-07-14 14:14:55 +0530

    refactor: refactored the app.blade

--- adminlte-stubs/layouts/app.blade.php
+++ adminlte-stubs/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
-    @yield('third_party_stylesheets')
+    @stack('third_party_stylesheets')
 
     @stack('page_css')
 </head>
@@ -81,7 +81,7 @@
 
 <script src="{{ mix('js/app.js') }}" defer></script>
 
-@yield('third_party_scripts')
+@stack('third_party_scripts')
 
 @stack('page_scripts')
 </body>
