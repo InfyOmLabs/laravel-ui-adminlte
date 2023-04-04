ae16aa41282ea9abd28f9e5235d9c5061cbe0fb9
Mitul Golakiya <mtl.golakiya@gmail.com>
2023-04-04 16:17:58 +0530

    fix: changed footer to 2023

--- adminlte-stubs/layouts/app.blade.php
+++ adminlte-stubs/layouts/app.blade.php
@@ -58,7 +58,7 @@
                 <div class="float-right d-none d-sm-block">
                     <b>Version</b> 3.0.5
                 </div>
-                <strong>Copyright &copy; 2014-2022 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
+                <strong>Copyright &copy; 2014-2023 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
                 reserved.
             </footer>
         </div>
